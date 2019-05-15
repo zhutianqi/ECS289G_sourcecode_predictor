6b53fbee012d6dc607b17928c93e04c2386dba01
secretsparkle <scott.madera@gmail.com>
2019-05-15 01:28:46 +0000

    Added beam search function

--- code/model.py
+++ code/model.py
@@ -36,6 +36,35 @@ class WordModel(tf.keras.Model):
                 results = tf.keras.backend.ctc_decode(preds, input_length, False, 5)
                 return results
 
+# Source: https://github.com/mmehdig/lm_beam_search
+def beam_search(model, src_input, k=1, sequence_max_len=25):
+        # (log(1), initialize_of_zeros)
+        k_beam = [(0, [0]*(sequence_max_len+1))]
+
+        # l : point on target sentence to predict
+        for l in range(sequence_max_len):
+            all_k_beams = []
+            for prob, sent_predict in k_beam:
+                predicted = model.predict([np.array([src_input]), np.array([sent_predict])])[0]
+                # top k!
+                possible_k = predicted[l].argsort()[-k:][::-1]
+
+                # add to all possible candidates for k-beams
+                all_k_beams += [
+                        (
+                            sum(np.log(predicted[i][sent_predict[i+1]]) for i in range(l)) \
+                                    + np.log(predicted[l][next_wid]), \
+                                    list(sent_predict[:l+1]) + [next_wid] + [0] * (sequence_max_len-l-1)
+                        )   
+                        for next_wid in possible_k
+                    ]
+
+            # top k
+            k_beam = sorted(all_k_beams)[-k:]
+
+        return k_beam
+
+
 def eval(model, data):
         mbs = 0
         count = 0
